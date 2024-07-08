057ad81fda316655e7975bb1e82ee81559196f82
MatheusCamargoGinebro <matheus.ginebro@aluno.ifsp.edu.br>
2024-07-08 18:06:56 -0300

    Tailwind-css add

--- electron/main.cjs
+++ electron/main.cjs
@@ -1,7 +1,15 @@
 const { app, BrowserWindow } = require("electron");
 
 function createWindow() {
-  const win = new BrowserWindow({ title: "IFLab", width: 1024, height: 768 });
+  const win = new BrowserWindow({
+    title: "IFLab",
+    width: 1280,
+    height: 720,
+    autoHideMenuBar: true,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
   win.loadURL("http://localhost:5173");
 }
 
