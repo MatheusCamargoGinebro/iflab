8086ee8032ce6c8c1ce3d03e317ee20532a78d0d
MatheusCamargoGinebro <matheus.ginebro@aluno.ifsp.edu.br>
2024-08-27 15:48:54 -0300

    chore: Update micromatch npm dependency to version 4.0.8 and add profile user context menu

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,27 +1,51 @@
 import logo from "../../assets/images/logo.png";
 import user from "../../assets/icons/UI/user.png";
+import user_settings from "../../assets/icons/UI/user-settings.png";
+import logout from "../../assets/icons/UI/logout.png";
+
+import { useState } from "react";
 
 function Header({ username, usertype, profilepic }) {
+  const [showUserMenu, setShowUserMenu] = useState(false);
+
   return (
-    <header className="bg-iflab_white w-full fixed px-10 flex items-center justify-between p-2 shadow-sm">
-      <div>
+    <header className="bg-iflab_white w-full fixed flex items-center justify-between shadow-sm">
+      <div className="mx-5 my-2">
         <img src={logo} alt="logo" className="h-14" />
       </div>
-      <div className="flex">
+
+      <div className="flex mx-5 my-2 group cursor-pointer" onClick={() => setShowUserMenu(!showUserMenu)}>
         <img
           src={profilepic ? profilepic : user}
           alt="Usuário"
-          className="w-12 h-12 border border-iflab_gray_light rounded-full p-1"
+          className="w-12 h-12 border border-iflab_gray_light group-hover:border-iflab_gray rounded-full p-1"
         />
         <div className="flex flex-col justify-center ml-1 text-[13px]">
-          <p className=" text-iflab_gray">
+          <p className=" text-iflab_gray group-hover:text-iflab_gray_dark">
             {username ? username : "Usuário de testes"}
           </p>
-          <p className=" text-iflab_gray_light">
+          <p className=" text-iflab_gray_light group-hover:text-iflab_gray">
             {usertype ? usertype : "Administrador"}
           </p>
         </div>
       </div>
+
+      {showUserMenu && (
+        <div className="fixed right-2 top-20">
+        <button className="bg-iflab_white p-3 hover:bg-iflab_white_dark border-b-iflab_gray_light border-b rounded-t-md flex items-center gap-2">
+          <img
+            src={user_settings}
+            alt="Configuração do usuário"
+            className="h-[32px]"
+          />
+          Configurações do usuário
+        </button>
+        <button className="bg-iflab_white p-3 hover:bg-iflab_white_dark rounded-b-md flex items-center gap-2 w-full">
+          <img src={logout} alt="Fazer logout" className="h-[32px]" />
+          Fazer logout
+        </button>
+      </div>
+      )}
     </header>
   );
 }
